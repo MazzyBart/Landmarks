da70cfc16ff246ae2963aba7d3c8e797959ca04c
Mazvydas Bartkevicius <hello@mazzy.uk>
2021-06-27 21:21:03 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mazzy Bart on 27/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
